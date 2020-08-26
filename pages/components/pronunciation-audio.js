69e4dd7300ba83f6b3ba9f03d2526cc34f81d241
Bahadır Tülüce <eminbahadir98@gmail.com>
2020-08-26 15:30:09 +0300

    Add text to speech

---
+++ pages/components/pronunciation-audio.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { Button, Intent, Toaster } from '@blueprintjs/core';
+
+const AudioState = { IDLE: 'idle', LOADING: 'loading', PLAYING: 'playing', FAILED: 'failed' };
+
+const PronunciationAudio = props => {
+  const [audioState, setAudioState] = useState(AudioState.IDLE);
+  const [audioInstance, setAudioInstance] = useState();
+  const [toasterRef, setToasterRef] = useState();
+
+  const playAudio = src => (
+    new Promise((resolve, reject) => {
+      const audio = new Audio();
+      audio.preload = 'auto';
+      audio.autoplay = true;
+      audio.onerror = reject;
+      audio.onended = resolve;
+      audio.src = src;
+      setAudioInstance(audio);
+    })
+  );
+
+  const fetchAndPlayAudio = async () => {
+    setAudioState(AudioState.LOADING);
+    const key = '86806895acac49db9b83acc4aecf4393';
+    const src = encodeURI(props.pronounceable);
+    const url = `https://api.voicerss.org/?key=${key}&hl=tr-tr&v=Omer&r=-3&f=32khz_16bit_stereo&b64=true&src=${src}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.text();
+      setAudioState(AudioState.PLAYING);
+      await playAudio(data);
+      setAudioState(AudioState.IDLE);
+    } catch (err) {
+      console.error(err);
+      setAudioState(AudioState.FAILED);
+      displayError();
+    }
+  };
+
+  const pauseAudio = () => {
+    if (audioInstance) {
+      audioInstance.pause();
+    }
+    setAudioState(AudioState.IDLE);
+  };
+
+  const displayError = () => {
+    if (toasterRef) {
+      toasterRef.show({
+        message: 'An error occured while loading the text-to-speech audio.',
+        icon: 'warning-sign',
+        intent: Intent.DANGER,
+      });
+    }
+  };
+
+  const handleClick = () => {
+    if (audioState === AudioState.IDLE) {
+      fetchAndPlayAudio();
+    } else if (audioState === AudioState.PLAYING) {
+      pauseAudio();
+    } else if (audioState === AudioState.FAILED) {
+      displayError();
+    }
+  };
+
+  const buttonIconMap = {
+    [AudioState.IDLE]: 'volume-up',
+    [AudioState.LOADING]: '',
+    [AudioState.PLAYING]: 'pause',
+    [AudioState.FAILED]: 'error',
+  };
+  const buttonIcon = buttonIconMap[audioState];
+
+  return (
+    <>
+      <Button
+        onClick={handleClick}
+        icon={buttonIcon}
+        loading={audioState === AudioState.LOADING}
+      />
+      <Toaster
+        ref={ref => setToasterRef(ref)}
+        maxToasts={1}
+      />
+    </>
+  );
+};
+
+export default PronunciationAudio;
---
+++ pages/components/pronunciation.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { Button, Popover, Position, Icon } from '@blueprintjs/core';
+import PronunciationAudio from './pronunciation-audio';
+
+const Pronunciation = props => {
+  const wordAnalysis = props.wordAnalysis;
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const pronunciations = wordAnalysis?.pronunciations?.slice(0, 50);
+
+  const maxIndex = pronunciations?.length - 1;
+  const minIndex = 0;
+  const actualIndex = Math.min(Math.max(activeIndex, minIndex), maxIndex);
+  const pronunciation = pronunciations[actualIndex];
+
+  const goNext = () => {
+    setActiveIndex(Math.min(maxIndex, actualIndex + 1));
+  };
+
+  const goPrevious = () => {
+    setActiveIndex(Math.max(minIndex, actualIndex - 1));
+  };
+
+  const renderExplanation = word => {
+    const source = pronunciation.details[word].source;
+    const from = pronunciation.details[word].explanation.from;
+    const to = pronunciation.details[word].explanation.to;
+    if (source === 'dictionary') {
+      const url = `https://dictionary.cambridge.org/dictionary/english/${word}`;
+      return (
+        <span>
+          (<a href={url} target='_blank' rel='noreferrer'>dictionary <Icon icon='manual'/></a> )
+        </span>
+      );
+    }
+    if (source === 'syllable-translation') {
+      return (
+        <span>
+          ({from} &#8594; {to})
+        </span>
+      );
+    }
+    if (source === 'letter-translation') {
+      return (
+        <span>
+          ({from} &#8594; {to})
+        </span>
+      );
+    }
+  };
+
+  return (
+    <span>
+      <Popover position={Position.BOTTOM} content={(
+        <div className='pornunciation-popover'>
+          <PronunciationAudio pronounceable={wordAnalysis.pronounceable}/>
+          &nbsp;
+          {wordAnalysis.pronounceable}
+          &nbsp;&#8594;&nbsp;
+          <b>{pronunciation.display}</b>
+          &nbsp;({actualIndex + 1} / {pronunciations?.length})
+          <br/>
+          <br/>
+          {pronunciation.words.map((word, i) => (
+            <div className='pronunciation-word-line' key={i}>
+              <b>{word}</b> {renderExplanation(word)}
+            </div>
+          ))}
+          <br/>
+          <Button
+            icon='arrow-left'
+            text='Previous'
+            onClick={goPrevious}
+          />
+          &nbsp;
+          <Button
+            rightIcon='arrow-right'
+            text='Next'
+            onClick={goNext}
+          />
+        </div>
+      )}>
+        <Button className='pronunciation-button'>{pronunciation.display}</Button>
+      </Popover>
+    </span>
+  );
+};
+
+export default Pronunciation;
