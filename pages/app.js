84b0ad5b4b68acae961ce2ab10a7f5e2d4e7b421
Bahadır Tülüce <eminbahadir98@gmail.com>
2020-08-26 19:16:08 +0300

    Add about text

--- pages/app.js
+++ pages/app.js
@@ -7,13 +7,13 @@ import GithubIcon from './components/github-icon';
 
 const exampleTexts = [
   'Merhaba dünya!',
+  'Emin Bahadır Tülüce',
   'Antik diyarlardan bir gezgine rastladım.',
+  'İhsan Doğramacı Bilkent Üniversitesi',
   'Sabah reçel yemek istiyorum.',
   'İşletim sistemleri, veritabanları ve nesne yönelimli programlama',
-  'Sıradaki sefer saat kaçta?',
-  'İhsan Doğramacı Bilkent Üniversitesi',
-  'Emin Bahadır Tülüce',
   'Türkiye Cumhuriyeti',
+  'Sıradaki sefer saat kaçta?',
 ];
 
 const App = () => {
@@ -51,25 +51,24 @@ const App = () => {
 
   return (
     <div className='app'>
-      <div className='justifier-container'>
-        <h1>Pronounce TR</h1>
-        <div>
-          <div className='app-extra-buttons'>
-            <Button
-              icon='help'
-              text='About'
-              onClick={() => setIsDrawerOpen(true)}
-            />
-            &nbsp;
-            <AnchorButton
-              icon={<GithubIcon />}
-              href='https://github.com/tuluce/pronounce-tr'
-              target='_blank'
-              rel='noreferrer'
-            />
-          </div>
+      <h1>
+        Pronounce TR&nbsp;
+        <div className='meta-buttons-wrapper'>
+          <Button
+            className='meta-button'
+            icon='help'
+            onClick={() => setIsDrawerOpen(true)}
+          />
+          &nbsp;
+          <AnchorButton
+            className='meta-button'
+            icon={<GithubIcon />}
+            href='https://github.com/tuluce/pronounce-tr'
+            target='_blank'
+            rel='noreferrer'
+          />
         </div>
-      </div>
+      </h1>
       <div>
         <div>
           <p>Enter the Turkish phrase you want to pronounce.</p>
@@ -104,7 +103,28 @@ const App = () => {
         title='About Pronounce TR'
       >
         <div className='drawer-content'>
-          <h2>What is it?</h2>
+          <h3>What?</h3>
+          <p>
+            An open-source web app to help with the pronunciation of Turkish words. To check
+            out the source code or contribute, please visit the GitHub repository.
+          </p>
+          <h3>Why?</h3>
+          <p>
+            Unlike any other pronunciation dictionaries, Pronounce TR attempts to find
+            English dictionary words which have close pronunciations to the target Turkish
+            syllable. The goal is to make the prounciation easier to the people who are not
+            familiar with Turkish.
+          </p>
+          <h3>How?</h3>
+          <p>
+            Pronounce TR uses the CMUdict (the Carnegie Mellon Pronouncing Dictionary) to
+            find English matches to Turkish syllables. Dividing Turkish words into syllables
+            is done in various ways. You can check out the documentation for the full explanation.
+          </p>
+          <br />
+          <hr />
+          <br />
+          <i>Emin Bahadır Tülüce - 2020</i>
         </div>
       </Drawer>
     </div>
